f3142ea4a1d88ec5af768ca18f65c0048e883127
Sameshnee21 <2490510@students.wits.ac.za>
2024-05-15 21:57:33 +0200

    navbar design

--- spa/src/Components/Navigation.jsx
+++ spa/src/Components/Navigation.jsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css"
 
 function Navigation() {
   return (
-    <>
-      <Link to={"/"}>Home</Link>
+    <div className="nav">
+
+      <div className="home">
+      <Link to={"/"}>Logo Here</Link>
+      </div>
+
+      <div className="links">
+      
       <Link to={"/track"}>The Landscape</Link>
       <Link to={"/history"}>History</Link>
       <Link to={"/styleguide"}>Styleguide</Link>
       <Link to={"/essay"}>Essay</Link>
       <Link to={"/artwork"}>Artwork</Link>
-    </>
+
+      </div>
+
+    </div>
+    
   );
 }
 
