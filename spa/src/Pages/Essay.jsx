d4c98c26508a053458f60ed5ed34f494da05485d
Sameshnee21 <2490510@students.wits.ac.za>
2024-05-17 21:15:37 +0200

    Update Essay.jsx

--- spa/src/Pages/Essay.jsx
+++ spa/src/Pages/Essay.jsx
@@ -3,6 +3,7 @@ function Essay() {
       <>
         <h1>Net/ Web Art</h1>
         <h3>A Critical Analysis of "Indirect Flights" by Joe Hamilton (2014)</h3>
+        <p>blah blah blah </p>
       </>
     );
   }
