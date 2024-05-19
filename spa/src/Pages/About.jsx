2549ac35eae18164fec2c85f321314ba92709cb9
Sameshnee21 <2490510@students.wits.ac.za>
2024-05-19 21:43:31 +0200

    styling 2

--- spa/src/Pages/About.jsx
+++ spa/src/Pages/About.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "../style/home.css";
+import "../style/about.css";
+import profile from "../Images/Portrait.png";
+
+
 
 function About() {
     return (
       <div>
         <h1 className="Title">Meet The Designer: Sameshnee Moodley</h1>
-        <img src="../Images/Portrait.png" alt="Biker with helmet" />
+        <img className="pic" src={profile} alt="Biker with helmet with a heart sign." />
+        <p className="Profile">Hey there! </p>
         </div>
 
         
